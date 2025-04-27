95b22b5225a185791de49f2d572116b7299876e6
Rauf Ahmad Khan <raufahmadkhan9@gmail.com>
2025-04-27 12:18:40 +0500

    Update code.java

--- calculator/Cli_calculator_1/code.java
+++ calculator/Cli_calculator_1/code.java
@@ -3,12 +3,13 @@ package ProdigyRaufCollection.calculator.Cli_calculator_1;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class code {
+public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your choice....\n1.Enter\n2.Exit");
-        int choice = sc.nextInt();
+        int choice = 0;
         do {
+            System.out.println("Enter your choice....\n1.Enter\n2.Exit");
+            choice = sc.nextInt();
             double result = 0;
             if (choice == 1) {
                 System.out.println("Enter number :");
@@ -17,35 +18,30 @@ public class code {
                 float b = sc.nextFloat();
                 System.out.println("Enter operation(+,-,*,/) :");
                 char operation = sc.next().charAt(0);
-                switch (operation) {
-                    case '+':
-                        result = a + b;
-                        break;
-                    case '-':
-                        result = a - b;
-                        break;
-                    case '*':
-                        result = a * b;
-                        break;
-                    case '/':
-                        if (b != 0) {
+                if (operation == '+' || operation == '-' || operation == '*' || (operation == '/' && b != 0)) {
+                    switch (operation) {
+                        case '+':
+                            result = a + b;
+                            break;
+                        case '-':
+                            result = a - b;
+                            break;
+                        case '*':
+                            result = a * b;
+                            break;
+                        case '/':
                             result = a / b;
-                        } else if (b == 0) {
-                            System.out.println("Can't divide by zero");
-                        }
-                        break;
-                    default:
-                        System.out.println("Incorrect choice");
+                            break;
+                        default:
+                    }
+                    DecimalFormat df = new DecimalFormat("0.####");
+                    System.out.println("The result is :" + df.format(result));
+                } else {
+                    System.out.println("Incorrect choice");
                 }
-                DecimalFormat df = new DecimalFormat("0.####");
-                System.out.println("The result is :" + df.format(result));
-                System.out.println("Enter your choice....\n1.Enter\n2.Exit");
-                choice = sc.nextInt();
             }
         } while (choice != 2);
-        if (choice == 2) {
-            System.out.println("Thanks for using our calculator system");
-        }
+        System.out.println("Thanks for using our calculator system");
         sc.close();
     }
 }
